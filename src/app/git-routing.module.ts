eb6efa4b8642341ad61914ad0aa67552d48d64b8
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-21 11:08:25 +0300

    add routing

---
+++ src/app/git-routing.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Routes } from '@angular/router';
+
+
+import { GitFormComponent } from './git-form/git-form.component'; 
+
+const routes:Routes = [
+	{ path:"gitUser",component:GitFormComponent },
+]
+
+@NgModule({
+    imports: [
+    CommonModule,
+    RouterModule.forRoot(routes)
+],
+exports: [
+   RouterModule
+]
+})
+
+
+export class GitRoutingModule { }
