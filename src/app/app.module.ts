5c4b9f221c76e15341714447af9c2f9c592ad9f4
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-21 13:09:18 +0300

    days count

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,31 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {HttpClientModule} from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { GitRoutingModule } from './git-routing.module';
 import { AppComponent } from './app.component';
 import { GitFormComponent } from './git-form/git-form.component';
 import { GitSearchResultsComponent } from './git-search-results/git-search-results.component';
 import { DateFormatPipe } from './date-format.pipe';
+import { SearchGithubService } from './search-github.service';
+import { DaysCountPipe } from './days-count.pipe'
 
 @NgModule({
   declarations: [
     AppComponent,       
     GitFormComponent,
     GitSearchResultsComponent,
-    DateFormatPipe
+    DateFormatPipe,
+    DaysCountPipe
   ],
   imports: [      
     BrowserModule,
-    HttpClientModule, 
+    HttpClientModule,
+    FormsModule,
+    GitRoutingModule, 
     
   ],
-  providers: [],
+  providers: [SearchGithubService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
