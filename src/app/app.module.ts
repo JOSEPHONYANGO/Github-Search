be66e0fbac36cbc32b082fbb9aea7b4dc3c71b10
JOSEPHONYANGO <joseph.onyango@student.moringaschool.com>
2022-04-21 10:42:53 +0300

    add user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,29 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { ApiService } from './api.service';
 import {HttpClientModule} from '@angular/common/http'
 import { AppComponent } from './app.component';
-import { UsersComponent } from './users/users.component';
-import { GhUserComponent } from './gh-user/gh-user.component';
-import { FormComponent } from './form/form.component';
 import { GitFormComponent } from './git-form/git-form.component';
 import { GitSearchResultsComponent } from './git-search-results/git-search-results.component';
+import { DateFormatPipe } from './date-format.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    UsersComponent,
-    GhUserComponent,
-    FormComponent,
+    AppComponent,       
     GitFormComponent,
-    GitSearchResultsComponent
+    GitSearchResultsComponent,
+    DateFormatPipe
   ],
   imports: [      
     BrowserModule,
     HttpClientModule, 
     
   ],
-  providers: [ApiService],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
